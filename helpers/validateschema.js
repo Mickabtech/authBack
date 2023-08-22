d172b56da07a894d0a5276ba9371556dd5851d8c
Mickabtech <Mickabtech@gmail.com>
2023-08-22 11:47:01 +0100

    Validation

--- helpers/validateschema.js
+++ helpers/validateschema.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
 const authSchema = Joi.object({
-    firstName: Joi.string().min(3).max(25).required(),
-    lastName: Joi.string().min(3).max(25).required(),
+    first_name: Joi.string().min(3).max(25).required(),
+    last_name: Joi.string().min(3).max(25).required(),
     email: Joi.string().email().lowercase().required(),
     password: Joi.string().min(8).required(),
     field: Joi.string().min(3).max(200).required(),
