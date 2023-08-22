31c909728e749ef3c6b8f9093f9b41353aed612a
Mickabtech <Mickabtech@gmail.com>
2023-08-22 11:51:42 +0100

    validation

--- model/user.js
+++ model/user.js
@@ -1,31 +1,23 @@
 const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-const Joi = require('joi');
+const Schema = mongoose.Schema;
+
 
 
 //creating a user model
-// const userSchema = new Schema({
-//   first_name: { type: String, default: null, required: true, },
-//   last_name: { type: String, default: null, required: true, },
-//   email: { type: String, unique: true, required: true, },
-//   password: { type: String, required: true, },
-//   field: { type: String, default: null, required: true, },
-//   profession: { type: String, default: null, required: true, },
-//   aboutYou: { type: String, default: null, required: true, },   
-//   token: { type: String }
-// });
+const userSchema = new Schema({
+  first_name: { type: String, default: null, required: true, },
+  last_name: { type: String, default: null, required: true, },
+  email: { type: String, unique: true, required: true, },
+  password: { type: String, required: true, },
+  field: { type: String, default: null, required: true, },
+  profession: { type: String, default: null, required: true, },
+  aboutYou: { type: String, default: null, required: true, },   
+  token: { type: String }
+});
 
 
 
-const userSchema = Joi.object({
-    first_name: Joi.string().min(3).max(25).required(),
-    last_name: Joi.string().min(3).max(25).required(),
-    email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(8).required(),
-    field: Joi.string().min(3).max(200).required(),
-    profession: Joi.string().min(3).max(200).required(),
-    aboutYou: Joi.string().min(20).max(1000).required(),
-})
+
 
 
 module.exports = mongoose.model("user", userSchema);
